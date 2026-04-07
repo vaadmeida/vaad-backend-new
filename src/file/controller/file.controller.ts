1eb2da19e2a6e8bbdb37e13cf9f0da077c9fe7a6
Gbenga Oyewole <gbengstar@gmail.com>
2026-04-07 07:36:46 +0100

    feat: multiple file uploads

--- src/file/controller/file.controller.ts
+++ src/file/controller/file.controller.ts
@@ -6,27 +6,27 @@ import {
   Logger,
   UploadedFiles,
 } from '@nestjs/common';
-import { FileInterceptor } from '@nestjs/platform-express';
+import { FileInterceptor, FilesInterceptor } from '@nestjs/platform-express';
 import { FileService } from '../service/file.service';
 import { ApiTags } from '@nestjs/swagger';
 
-@ApiTags('files')
+@ApiTags('Files')
 @Controller('files')
 export class FileController {
   private readonly logger = new Logger(FileController.name);
   constructor(private readonly fileService: FileService) {}
+
+  @Post('uploads/many')
+  @UseInterceptors(FilesInterceptor('files'))
+  async uploadFiles(@UploadedFiles() files: any[]) {
+    const results = await this.fileService.uploadFiles(files);
+    return { fileUrls: results.map((f) => f.url) };
+  }
 
   @Post('uploads')
   @UseInterceptors(FileInterceptor('file'))
   async uploadFile(@UploadedFile() file: any) {
     const result = await this.fileService.uploadFile(file);
     return { fileUrl: result.url };
-  }
-
-  @Post('uploads/many')
-  @UseInterceptors(FileInterceptor('file'))
-  async uploadFiles(@UploadedFiles() files: any[]) {
-    const results = await this.fileService.uploadFiles(files);
-    return { fileUrls: results.map((f) => f.url) };
   }
 }
